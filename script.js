6c9e4902051837321622376fcb68e9662e06f739
heegon <157395188+heegon02@users.noreply.github.com>
2025-02-21 12:45:02 +0900

    Update script.js
    남은 시간 (timeLeft)가 24시간이 넘어갈 때, hours 변수에 값이 제대로 할당되지 못하는 문제 해결.

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function startTimer() {
             return;
         }
     
-        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const hours = Math.floor(timeLeft / (1000 * 60 * 60));
         const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeLeft % (1000 * 60)) / (1000));
 
